d3e3ae0777d6c67d82f4efde5b7013dca7d2d060
LacrimaeAware <136732999+LacrimaeAware@users.noreply.github.com>
2024-06-22 15:05:32 -0400

    Exercise 1.2
    app.jsx update make part subc omponents

--- part1/1.1/src/App.jsx
+++ part1/1.1/src/App.jsx
@@ -6,15 +6,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+  <div>
+    <p>{props.part[0]} {props.part[1]}</p>
+  </div>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-
-      <p>{props.part2} {props.exercises2}</p>
-
-      <p>{props.part3} {props.exercises3}</p>
-
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
     </div>
   )
 }
@@ -27,18 +32,16 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = ['Fundamentals of React', 10]
+  const part2 = ['Using props to pass data', 7]
+  const part3 = ['State of a component', 14]
+  const parts = [part1, part2, part3]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1+exercises2+exercises3}/>
+      <Content parts={parts}/>
+      <Total total={part1[1]+part2[1]+part3[1]}/>
     </div>
   )
 }
